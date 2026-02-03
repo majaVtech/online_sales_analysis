39e3a19673f7cefb89fb0f41cc13db470d8943d4
Maja Divjak <majadivjak2003@gmail.com>
2026-02-03 12:06:10 +0100

    Added class Cart and updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 pm = ProductManager()
 pm.add_product(Product("Laptop", 80000, 5))
 pm.add_product(Product("Telefon", 50000, 10))
 pm.add_product(Product("Slusalice", 3000, 20))
 
-pm.show_products()
-print("Ukupna vrednost inventara:", pm.total_value(), "RSD")
+cart = Cart()
+cart.add_to_cart(pm.products[0])
+cart.add_to_cart(pm.products[1])
+cart.add_to_cart(pm.products[2])
+
+cart.show_cart()
+print("Ukupna vrijednost korpe:", cart.total_cart_value(), "RSD")
